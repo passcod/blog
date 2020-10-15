ac406d215fd19e74cd605483f0f426c8346892f9
Félix Saparelli <felix@passcod.name>
2020-10-16 04:11:25 +1300

    Fri 16 Oct 2020 04:11:25 NZDT

--- update.sh
+++ update.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 httrack --sockets=32 --mirror --update --disable-security-limits -X -N100 https://blog.nut
 
-rmdir f i t
+rmdir f i t || true
 mv index-2.html index.html
+mv feed feed.xml
 rg -lFg !update.sh index-2.html | xargs sed -i 's/index-2.html/index.html/g'
 rg -lFg !update.sh /feed | xargs sed -i 's|/feed"|/feed.xml"|g'
+sed -i 's|"feed"|"/feed.xml"|g' index.html
 echo '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=/index.html">' > index-2.html
 
 git add .
