b66c68102aaf4414bb67866a4ef099a0a337ba26
Félix Saparelli <felix@passcod.name>
2020-10-16 04:00:10 +1300

    Fri 16 Oct 2020 04:00:10 NZDT

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ httrack --sockets=32 --mirror --update --disable-security-limits -X -N100 https:
 rmdir f i t
 mv index.html index.html
 rg -lF index.html | xargs sed -i 's/index.html/index.html/g'
+rg -lF /feed | xargs sed -i 's|/feed.xml"|/feed.xml"|g'
 
 git add .
 git commit -am "$(date)"
