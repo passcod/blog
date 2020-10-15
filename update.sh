b6297078d11e327d77c56b94302c2fe4d1c28cfc
Félix Saparelli <felix@passcod.name>
2020-10-16 03:56:41 +1300

    Fri 16 Oct 2020 03:56:41 NZDT

--- update.sh
+++ update.sh
@@ -3,7 +3,11 @@
 set -euo pipefail
 
 httrack --sockets=32 --mirror --update --disable-security-limits -X -N100 https://blog.nut
+
 rmdir f i t
+mv index.html index.html
+rg -lF index.html | xargs sed -i 's/index.html/index.html/g'
+
 git add .
 git commit -am "$(date)"
 git push
